3cea0d26357d0f133a1f859e7980cc4a5df9c542
Jack Hubert <jphubert93@gmail.com>
2020-11-23 23:00:04 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,16 +1,32 @@
+require pry
+
 def find_element_index(array, value_to_find)
   counter = 0
   
   if array.include?(value_to_find)
+<<<<<<< HEAD
     while array[counter] do
       if array[counter] == value_to_find
         return counter
       end
       counter += 1
+=======
+    binding.pry
+    while array[counter] {
+      if array[counter] == value_to_find
+        binding.pry
+        return counter
+        # return array.index(element)
+      else
+        counter += 1
+      end
+    }
+>>>>>>> d07ef81d97fd6103a14313f01c094b4b52eef135
     end
   end
 end
 
+<<<<<<< HEAD
 # def find_element_index(array, value_to_find)
 #   count = 0
 #   while count < array.length do
@@ -24,4 +40,13 @@ end
 
 # def find_element_index(array, value_to_find)
 #   array.index(value_to_find)
+=======
+
+# counter = 0
+# pets = ["Dog", "Cat", "Fish", "Bird", "Hamster"]
+ 
+# while pets[counter] do
+#   puts pets[counter]
+#   counter += 1
+>>>>>>> d07ef81d97fd6103a14313f01c094b4b52eef135
 # end
